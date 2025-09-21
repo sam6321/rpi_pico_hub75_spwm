d56ac2d8d7fdd7f92b3867de689b737d5d49f71f
Illia Lykhoshvai <illialykhoshvai@gmail.com>
2025-09-21 13:06:06 +0300

    add gif decode library

--- main.c
+++ main.c
@@ -3,38 +3,82 @@
 #include "pico/multicore.h"
 #include "hardware/clocks.h"
 #include "led_panel.h"
-#include "image.h"
+#include "minigif.h"
+#include "gif.h"
 
-static inline void rgb565_conversion(uint16_t c, uint16_t *r, uint16_t *g, uint16_t *b)
+static mutex_t frame_buff_mtx;
+static uint16_t frame_buf_a[MATRIX_WIDTH * MATRIX_HEIGHT * 3];
+static uint16_t frame_buf_b[MATRIX_WIDTH * MATRIX_HEIGHT * 3];
+static uint16_t *display_buf = frame_buf_a;  // used by display_routine()
+static uint16_t *tmp_buf = frame_buf_b;      // used by painter_cb()
+
+typedef struct {
+    const uint8_t *data;
+    size_t size;
+    size_t pos;
+} memfile_t;
+
+static long _read_bytes_cb(void *f, uint8_t *val, size_t size)
 {
-    *r = ((((c) >> 11) & 0x1F) << 14) >> 5;
-    *g = ((((c) >>  5) & 0x3F) << 14) >> 6;
-    *b = ((((c) >>  0) & 0x1F) << 14) >> 5;
+    memfile_t *mem = (memfile_t *)f;
+    if (mem->pos + size > mem->size) {
+        size = mem->size - mem->pos; // Clamp read to available bytes
+    }
+
+    memcpy(val, mem->data + mem->pos, size);
+    mem->pos += size;
+    return size;
 }
 
-static uint16_t fb[128 * 64 * 3];
-
-static inline uint16_t image_get_pixel(uint x, uint y)
+static long _lseek_cb(void *f, long off, int seek)
 {
-    return ((uint16_t *)mountains_128x64)[y * 128 + x];
+    memfile_t *mem = (memfile_t *)f;
+
+    size_t new_pos = 0;
+    switch (seek) {
+        case SEEK_SET:
+            new_pos = off;
+            break;
+        case SEEK_CUR:
+            new_pos = mem->pos + off;
+            break;
+        case SEEK_END:
+            new_pos = mem->size + off;
+            break;
+        default:
+            return -1;
+    }
+
+    if (new_pos > mem->size) {
+        return -1;
+    }
+    
+    mem->pos = new_pos;
+    return (long)mem->pos;
+}
+
+static void _painter_cb(uint16_t x, uint16_t y, gif_rgb_t rgb, void *user_data) {
+    size_t idx = (y * MATRIX_WIDTH + x) * 3;
+    tmp_buf[idx + 0] = rgb.r << 6;
+    tmp_buf[idx + 1] = rgb.g << 6;
+    tmp_buf[idx + 2] = rgb.b << 6;
 }
 
 static void core1_entry() {
     dp3364_init();
     
-    while (1) 
-    {
+    while (1) {
+        mutex_enter_blocking(&frame_buff_mtx);
+        uint16_t *fb = display_buf;
+
         dp3364_vsync();
-        for (uint line = 0; line < 32; line++) 
-        { // Y
-            for (uint col = 0; col < 16; col++) 
-            { // channel in driver chip
-                for (uint ch = 1; ch <= DP3364_NUM_CHANNELS; ch++) 
-                { // each driver chip
+        for (uint line = 0; line < 32; line++) {
+            for (uint col = 0; col < 16; col++) { // column within driver chip
+                for (uint ch = 1; ch <= DP3364_NUM_CHANNELS; ch++) { // throughout each driver chip
                     uint8_t x = ((ch - 1) * 16) + col;   // global X (0..127)
 
                     // low half
-                    size_t idx_lo = (line * 128 + x) * 3;
+                    size_t idx_lo = (line * MATRIX_WIDTH + x) * 3;
                     uint16_t rgb_lo[3] = {
                         fb[idx_lo + 0],
                         fb[idx_lo + 1],
@@ -42,7 +86,7 @@ static void core1_entry() {
                     };
 
                     // high half
-                    size_t idx_hi = ((line + 32) * 128 + x) * 3;
+                    size_t idx_hi = ((line + 32) * MATRIX_WIDTH + x) * 3;
                     uint16_t rgb_hi[3] = {
                         fb[idx_hi + 0],
                         fb[idx_hi + 1],
@@ -63,7 +107,12 @@ static void core1_entry() {
                 clock_pulse();
             }
         }
+        mutex_exit(&frame_buff_mtx);
     }
+}
+
+static uint32_t get_time_ms() {
+    return to_ms_since_boot(get_absolute_time());
 }
 
 int main() {
@@ -81,17 +130,55 @@ int main() {
         gpio_set_slew_rate(i, GPIO_SLEW_RATE_FAST);
     }
 
-    // Ensure DP3364/DP32020 power rails are applied and stable after this point
-    busy_wait_ms(2);
-    
-    for (size_t i = 0; i < sizeof(mountains_128x64)/sizeof(uint16_t); i++) {
-        uint16_t c = image_get_pixel(i % 128, i / 128);
-        rgb565_conversion(c, &fb[i*3+0], &fb[i*3+1], &fb[i*3+2]);
+    memset(frame_buf_a, 0, sizeof(frame_buf_a));
+    memset(frame_buf_b, 0, sizeof(frame_buf_b));
+    mutex_init(&frame_buff_mtx);
+
+    static memfile_t gif_mem = {
+        .data = gif_file,
+        .size = sizeof(gif_file),
+        .pos = 0
+    };
+
+    static uint8_t gif_buf[sizeof(gif_t)];
+
+    gif_cb_t gif_cb = {
+        .read = _read_bytes_cb,
+        .lseek = _lseek_cb,
+        .painter = _painter_cb,
+        .user_data = NULL,
+    };
+
+    gif_handle_t gif = minigif_init(gif_buf, &gif_mem, gif_cb);
+    assert(gif != NULL);
+
+    static uint32_t core1_stack[256];
+    multicore_launch_core1_with_stack(core1_entry, core1_stack, 1024);
+
+    while (1) {
+        uint32_t t0 = get_time_ms();
+
+        gif_status_t ret = minigif_render_frame(gif);
+        // assert(ret != GIF_STATUS_ERROR);
+
+        if (ret == GIF_STATUS_GIF_END) {
+            minigif_rewind(gif);
+            minigif_render_frame(gif);
+        }
+
+        mutex_enter_blocking(&frame_buff_mtx);
+        uint16_t *old = display_buf;
+        display_buf = tmp_buf;
+        tmp_buf = old;
+        mutex_exit(&frame_buff_mtx);
+
+        uint32_t t1 = get_time_ms();
+        uint32_t delta = t1 - t0;
+        uint32_t gif_delay_ms = minigif_get_frame_delay(gif) * 10;
+        uint32_t task_delay_ms = gif_delay_ms > delta ? gif_delay_ms - delta : 1;
+        
+        busy_wait_ms(task_delay_ms);
     }
-
-    multicore_launch_core1(core1_entry);
-
-    while(1);
 
     return 0;
 }
